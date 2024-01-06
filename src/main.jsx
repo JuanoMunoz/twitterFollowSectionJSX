3f49373dba026a5410ecc2cb2343c5c4ba05537a
Juan Muñoz <125042734+JuanoMunoz@users.noreply.github.com>
2024-01-06 11:03:23 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './assets/app';
+import { App } from './assets/App.jsx';
 import './css/output.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
